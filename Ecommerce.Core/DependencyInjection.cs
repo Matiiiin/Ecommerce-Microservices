8d841cc572e8543cb7a889041863f685aee3c06d
Matiiiin <matinnjt2000@gmail.com>
2026-09-10 14:52:45 +0330

    feat:migrating from data annotations to fluent validation for dtos

--- Ecommerce.Core/DependencyInjection.cs
+++ Ecommerce.Core/DependencyInjection.cs
@@ -5,8 +5,11 @@ using Ecommerce.Core.ServiceContracts.JWTToken;
 using Ecommerce.Core.Services.ApplicationUserSecurity;
 using ECommerce.Core.Services.Authentication;
 using Ecommerce.Core.Services.JWTToken;
+using Ecommerce.Core.Validators.DTO.AuthenticationDTO;
+using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 namespace Ecommerce.Core;
 
@@ -17,7 +20,9 @@ public static class DependencyInjection
         services.AddScoped<IPasswordCheckerServiceContract, PasswordCheckerService>();
         services.AddScoped<IAuthenticationServiceContract, AuthenticationService>();
         services.AddScoped<IJWTTokenServiceContract, JWTTokenService>();
-        
+        services.AddValidatorsFromAssemblyContaining<RegistrationDTOValidator>();
+        services.AddFluentValidationAutoValidation();
+
         
         
         services.Configure<JWTOptions>(
---
+++ Ecommerce.Core/Validators/DTO/AuthenticationDTO/LoginDTOValidator.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Core.DTO.AuhenticationDTO;
+using FluentValidation;
+
+namespace Ecommerce.Core.Validators.DTO.AuthenticationDTO;
+
+public class LoginDTOValidator : AbstractValidator<LoginDTO>
+{
+    public LoginDTOValidator()
+    {
+        //Email
+        RuleFor(loginDTO => loginDTO.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .NotNull()
+            .EmailAddress().WithMessage("Provide a valid email address");
+        
+        //password
+        RuleFor(LoginDTO => LoginDTO.Password)
+            .NotEmpty()
+            .WithMessage("Password is required")
+            .NotNull();
+    }
+}
